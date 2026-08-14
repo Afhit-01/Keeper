d0942c2daa26f4bca7997f038b4d153a6e46675b
Fatihu A. AbdulGaniyu <abdulghaniyyfaatih2@gmail.com>
2026-08-14 08:24:32 +0100

    Set up the basic interface MVP for project Keeper

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./header";
+import Note from "./note";
+import Footer from "./footer";
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <Note />
+    <Footer />
+    </>
+  )
+}
+
+export default App;
---
+++ src/note.jsx
@@ -0,0 +1,12 @@
+const Note = () => {
+    return (
+        <>
+        <div className="note">
+            <h1>Title</h1>
+            <p>Start typing your notes</p>
+        </div>
+        </>
+    )
+}
+
+export default Note;
